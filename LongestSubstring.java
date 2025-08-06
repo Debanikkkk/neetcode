f5ae7ef2160f5090f323cda700542d761d14362d
Debanik Pahilwani THINKPAD <debanikpahilwani@gmail.com>
2025-08-06 18:13:04 +0530

    sorting started + longest substring wihtout repeating characters started

---
+++ LongestSubstring.java
@@ -0,0 +1,13 @@
+
+import java.util.HashSet;
+
+public class LongestSubstring {
+    static int longestSubstringLengthBF(String s){
+     
+    }
+    public static void main(String[] args) {
+        String s="abcabcbb";
+        System.out.println(longestSubstringLengthBF(s));
+
+    }
+}
---
+++ sorting/SelectionSort.java
@@ -0,0 +1,17 @@
+
+
+public class SelectionSort {
+    // static int findSmallest(){
+
+    // }
+
+    static void selectionSort(int[] ar){
+    }
+    public static void main(String[] args) {
+        int [] ar=new int[]{4,1,5,2,44,1,45};
+        selectionSort(ar);
+        for(int a: ar){
+            System.out.println(a+", ");
+        }        
+    }
+}
